ab3b825c4bd36df3fc86ad26e0267255333e110a
URAYAMA,kazuaki <kazuaki.urayama@gmail.com>
2018-05-15 11:10:01 +0900

    introduced Test Suite TS1

---
+++ Test Suites/TS1.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-05-15T11:00:40</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>b432e51e-34c7-42fb-a6f5-58951b3335a0</testSuiteGuid>
+   <testCaseLink>
+      <guid>2d755f7c-68ba-438b-8327-dde73fce4bae</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Main/LoginTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8209f260-6286-4128-b632-2354a0936883</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Main/Basic</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
